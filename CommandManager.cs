0aaf35c707df93d248c70a3c3d6fb62974d84a78
Txxxthless <lepash85@gmail.com>
2023-02-12 11:40:17 +0200

    Templates for new class and interface.

---
+++ CommandManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendableConsoleApplication
+{
+    internal static class CommandManager
+    {
+        private static List<ICommand> Commands = new List<ICommand>();
+
+        public static void GetCommands()
+        {
+            foreach (var command in Commands)
+            {
+
+            }
+        }
+    }
+}
